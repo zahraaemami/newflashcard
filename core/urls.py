7c82cb963f639d57a78bed7a35b3435ff6b1fd6c
zahra <zahraaeemami@gmail.com>
2024-11-17 15:21:17 +0330

    add a token and refresh token

--- core/urls.py
+++ core/urls.py
@@ -21,7 +21,7 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('auth' , include('Authentication.urls'))
+    path('auth/' , include('Authentication.urls')),
 ]
 
 
